6b3e816b1635cdd9e0d6e17cf554e7302f737d8c
张双男 <zhangshuangnan@zuoyebang.com>
2017-10-19 14:54:43 +0800

    nullpointerexception

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobFS.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobFS.java
@@ -213,7 +213,9 @@ public class RNFetchBlobFS {
         state = Environment.getExternalStorageState();
         if (state.equals(Environment.MEDIA_MOUNTED)) {
             res.put("SDCardDir", Environment.getExternalStorageDirectory().getAbsolutePath());
-            res.put("SDCardApplicationDir", ctx.getExternalFilesDir(null).getParentFile().getAbsolutePath());
+            try{
+                res.put("SDCardApplicationDir", ctx.getExternalFilesDir(null).getParentFile().getAbsolutePath());
+            }catch(NullPointerException ignored){}
         }
         res.put("MainBundleDir", ctx.getApplicationInfo().dataDir);
         return res;
